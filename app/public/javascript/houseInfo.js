02012523c3078cd4e7e758fc85723bd91fc17537
Misha Kushka <kushka.misha@gmail.com>
2021-04-23 18:34:30 +0300

    Both pages are getting houses well

---
+++ app/public/javascript/houseInfo.js
@@ -0,0 +1,32 @@
+window.houseInfo = {
+  1: {
+    title: 'Atlanta, GA',
+    description:
+      'MULTIPLE OFFERS: highest and best due by 4/19/2021 at 5pm. Beautiful 4 bedroom, 2.5 bathroom, two-story home located in Azalea Cove Estates in Orlando. This home features a formal ...',
+  },
+  2: {
+    title: 'Scottsdale, AZ',
+    description:
+      'Lovely 3 bedroom, 2 bath home in Chatham Walk.  The single story home has an open living and dining room.  The kitchen boasts ample cabinet and counter space, including a closet pantry...',
+  },
+  3: {
+    title: 'Oakland, CA',
+    description:
+      'Looking for a single-story home in Azle? This is the one! 4 bedrooms, 2 full bath, completely repainted inside with new carpet! Spacious living room features an inviting wood-burning...',
+  },
+  4: {
+    title: 'Sandy Springs, GA',
+    description:
+      'Large 5 bedroom, 2 1/2 bathroom in Elk Grove with no HOA! Walk into a large family room with recessed lighting, laminate flooring, and large window for great natural light...',
+  },
+  5: {
+    title: 'Carlsbad, CA',
+    description:
+      'Cozy 3 bedroom, 2 bathroom, single-family home in the community of Townhill in Eustis. This home features an open living and dining room combination with laminate floors, vaulted...',
+  },
+  6: {
+    title: 'Newton, MA',
+    description:
+      'This stunning custom-built 3 bedroom, 2 bathroom brick home is located in the coveted gated community of Alamo Ranch. This functional open floor plan boasts a freshly painted interior...',
+  },
+}
--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -1,35 +1,46 @@
 const web3 = new window.Web3('https://rinkeby.infura.io/v3/b2d48917ee7f4f5dbe048259c3a8a554')
 
-web3.eth.getBlockNumber((err, res) => {
-  if (err) console.log({ err })
-  else console.log({ res })
-})
-
-getRegistryInstance()
+// // To test that the connection is OK
+// web3.eth.getBlockNumber((err, res) => {
+//   if (err) console.log({ err })
+//   else console.log({ res })
+// })
 
 async function getEscrowInstanceByAddress(address) {
   return new web3.eth.Contract(window.escrowAbi, address)
 }
 
-async function getRegistryInstance() {
+async function getAHouse(escrowAddr) {
+  const escrow = await getEscrowInstanceByAddress(escrowAddr)
+
+  const seller = await escrow.methods.seller().call()
+  const intermediate = await escrow.methods.intermediate().call()
+  const houseId = await escrow.methods.houseId().call()
+  const houseAddress = await escrow.methods.houseAddress().call()
+  const price = await escrow.methods.price().call()
+
+  return {
+    seller,
+    intermediate,
+    houseId,
+    houseAddress,
+    price,
+  }
+}
+
+async function getHouses() {
   const registryAddr = '0x56024F89Ce457a653aa775626670B0A3aE5A44E8'
   const registry = new web3.eth.Contract(window.registryAbi, registryAddr)
   const numOfEscrow = await registry.methods.getNumOfEscrow().call()
   console.log({ numOfEscrow })
 
-  // const escrowAddrs = []
   const houses = []
+
   for (let i = 0; i < numOfEscrow; i++) {
-    // eslint-disable-next-line no-await-in-loop
     const escrowAddr = await registry.methods.escrowAddrs(i).call()
     console.log({ escrowAddr })
     const escrow = await getEscrowInstanceByAddress(escrowAddr)
 
-    // const seller = '0x4D07e28E9EE6DC715b98f589169d7927239d7318'
-    // const intermediate = '0x6d82eB95C3c3468E1815242AB375327903E5261e'
-    // const houseId = 1
-    // const houseAddress = '401 Seventh Avenue, New York, NY'
-    // const price = 250
     const seller = await escrow.methods.seller().call()
     const intermediate = await escrow.methods.intermediate().call()
     const houseId = await escrow.methods.houseId().call()
@@ -45,9 +56,8 @@ async function getRegistryInstance() {
       houseAddress,
       price,
     })
-    // escrowAddrs.push(escrowAddr)
   }
-  console.log({ houses })
+  return houses
 }
 
 async function initialize(provider) {
@@ -56,55 +66,3 @@ async function initialize(provider) {
   const balance = await provider.request({ method: 'eth_getBalance', params: [accounts[0]] })
   console.log({ balance })
 }
-
-// async function deployContract() {
-//   const sellForm = document.getElementById('sellHouse')
-//   const data = new FormData(sellForm)
-
-//   console.log({ data })
-// }
-
-// const testForm = document.getElementById('sellHouse')
-// testForm.onsubmit = (event) => {
-//   event.preventDefault()
-
-//   const request = new XMLHttpRequest()
-
-//   request.onreadystatechange = function () {
-//     if (this.readyState === 4 && this.status === 200) {
-//       console.log(this.response)
-//     }
-//   }
-
-//   const formData = new FormData(document.getElementById('sellHouse'))
-//   const data = {
-//     seller: formData.get('seller'),
-//     intermediate: formData.get('intermediate'),
-//     houseId: formData.get('houseId'),
-//     houseAddress: formData.get('houseAddress'),
-//     price: formData.get('price'),
-//   }
-
-//   request.open('POST', '/deployContract', /* async = */ true)
-//   request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-//   request.send(JSON.stringify(data))
-// }
-
-// window.addEventListener('load', async () => {
-//   const provider = await window.detectEthereumProvider()
-
-//   if (provider) {
-//     provider.autoRefreshOnNetworkChange = false
-//     initialize(provider)
-//   } else {
-//     console.error('Please install MetaMask!')
-//   }
-// })
-
-const ethereumButton = document.querySelector('.enableEthereumButton')
-
-ethereumButton.addEventListener('click', async () => {
-  // Will Start the metamask extension
-  console.log(await window.ethereum.request({ method: 'eth_requestAccounts' }))
-  // initialize
-})
